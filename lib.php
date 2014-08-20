a6d870dc5add470b346a86c9478ff3961f6d9d82
3i Logic LMS <lms@3ilogic.com>
2014-08-20 10:05:11 +0500

    Update lib.php

--- lib.php
+++ lib.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -13,13 +14,12 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * Theme version info
  *
  * @package    theme
  * @subpackage sharp
- * @copyright � 2012 - 2013 | 3i Logic (Pvt) Ltd.
+ * @copyright � 2012 - 2014 | 3i Logic (Pvt) Ltd.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 function sharp_set_linkcolor($css, $linkcolor) {
@@ -27,6 +27,56 @@ function sharp_set_linkcolor($css, $linkcolor) {
     $replacement = $linkcolor;
     if (is_null($replacement)) {
         $replacement = '#2d83d5';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+function sharp_set_gradientstart($css, $gradientstart) {
+    $tag = '[[setting:gradientstart]]';
+    $replacement = $gradientstart;
+    if (is_null($replacement)) {
+        $replacement = '#30a1bf';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+function sharp_set_gradientend($css, $gradientend) {
+    $tag = '[[setting:gradientend]]';
+    $replacement = $gradientend;
+    if (is_null($replacement)) {
+        $replacement = '#e2e2e2';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+function sharp_set_buttonbg($css, $buttonbg) {
+    $tag = '[[setting:buttonbg]]';
+    $replacement = $buttonbg;
+    if (is_null($replacement)) {
+        $replacement = '#30a1bf';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+function sharp_set_buttoncolor($css, $buttoncolor) {
+    $tag = '[[setting:buttoncolor]]';
+    $replacement = $buttoncolor;
+    if (is_null($replacement)) {
+        $replacement = '#ffffff';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+function sharp_set_buttonhovercolor($css, $buttonhovercolor) {
+    $tag = '[[setting:buttonhovercolor]]';
+    $replacement = $buttonhovercolor;
+    if (is_null($replacement)) {
+        $replacement = '#000000';
     }
     $css = str_replace($tag, $replacement, $css);
     return $css;
@@ -49,6 +99,40 @@ function sharp_process_css($css, $theme) {
         $linkcolor = null;
     }
     $css = sharp_set_linkcolor($css, $linkcolor);
+    if (!empty($theme->settings->gradientstart)) {
+        $gradientstart = $theme->settings->gradientstart;
+    } else {
+        $gradientstart = null;
+    }
+    $css = sharp_set_gradientstart($css, $gradientstart);
+    if (!empty($theme->settings->gradientend)) {
+        $gradientend = $theme->settings->gradientend;
+    } else {
+        $gradientend = null;
+    }
+    $css = sharp_set_gradientend($css, $gradientend);
+
+    if (!empty($theme->settings->buttonbg)) {
+        $buttonbg = $theme->settings->buttonbg;
+    } else {
+        $buttonbg = null;
+    }
+    $css = sharp_set_buttonbg($css, $buttonbg);
+
+    if (!empty($theme->settings->buttoncolor)) {
+        $buttoncolor = $theme->settings->buttoncolor;
+    } else {
+        $buttoncolor = null;
+    }
+    $css = sharp_set_buttoncolor($css, $buttoncolor);
+
+    if (!empty($theme->settings->buttonhovercolor)) {
+        $buttonhovercolor = $theme->settings->buttonhovercolor;
+    } else {
+        $buttonhovercolor = null;
+    }
+    $css = sharp_set_buttonhovercolor($css, $buttonhovercolor);
+
     if (!empty($theme->settings->customcss)) {
         $customcss = $theme->settings->customcss;
     } else {
