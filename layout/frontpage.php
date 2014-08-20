3ca31a3cbfe5331e67de71187c80cedabe06effc
3i Logic LMS <lms@3ilogic.com>
2014-08-20 09:59:23 +0500

    Update frontpage.php

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * Theme version info
  *
@@ -28,7 +27,6 @@ $haslogo = (!empty($PAGE->theme->settings->logo));
 $hasfooter = (empty($PAGE->layout_options['nofooter']));
 $hassidepost = $PAGE->blocks->region_has_content('side-post', $OUTPUT);
 $showsidepost = ($hassidepost && !$PAGE->blocks->region_completely_docked('side-post', $OUTPUT));
-
 $custommenu = $OUTPUT->custom_menu();
 $hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custommenu));
 $bodyclasses = array();
@@ -40,135 +38,109 @@ if ($showsidepost) {
 if ($hascustommenu) {
     $bodyclasses[] = 'has_custom_menu';
 }
-
-
 if (!empty($PAGE->theme->settings->tagline)) {
     $tagline = $PAGE->theme->settings->tagline;
 } else {
     $tagline = "";
 }
-
 if (!empty($PAGE->theme->settings->footertext)) {
     $footnote = $PAGE->theme->settings->footertext;
 } else {
     $footnote = '<!-- There was no custom footnote set -->';
 }
-
-
-echo $OUTPUT->doctype() ?>
+echo $OUTPUT->doctype()
+?>
 <html <?php echo $OUTPUT->htmlattributes() ?>>
-<head>
-    <title><?php echo $PAGE->title ?></title>
-    <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
-    <meta name="description" content="<?php p(strip_tags(format_text($SITE->summary, FORMAT_HTML))) ?>" />
-    <?php echo $OUTPUT->standard_head_html() ?>
-</head>
-
-<body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses.' '.join(' ', $bodyclasses)) ?>">
-<?php echo $OUTPUT->standard_top_of_body_html() ?>
-
-<div id="page">
-
-  <div id="page-wrap1">
-    <div id="page-wrap2">
-
-	<div id="wrapper" class="clearfix">
-
-<!-- START OF HEADER -->
-
-	    <div id="page-header">
-			<div id="page-header-wrapper" class="wrapper clearfix">
-
-	    	    <div id="headermenus" class="clearfix">
-	    	    	<div class="headermenu clearfix">
-	        		<?php
-						echo $OUTPUT->lang_menu();
-						echo $OUTPUT->login_info();
-						echo $PAGE->headingmenu;
-					?>
-			    	</div>
-		    	    <?php if ($hascustommenu) { ?>
-						<div id="custommenu"><?php echo $custommenu; ?></div>
-					<?php
-} else { ?>
-						<div id="custommenu" style="line-height:1em;">&nbsp;</div> <!-- temporary until I find a better fix -->
-					<?php 
-} ?>
-
-	    		</div>
-
-	    	</div>
-	    </div>
-
-<!-- END OF HEADER -->
-
-<!-- START OF CONTENT -->
-
-		<div id="page-content-wrapper" class="wrapper clearfix">
-		    <div id="page-content">
-    		    <div id="region-main-box">
-        		    <div id="region-post-box">
-
-            		    <div id="region-main-wrap">
-                		    <div id="region-main">
-                    		    <div class="region-content">
-
-									<div id="region-header">									
-									<?php if ($haslogo) {
-										echo html_writer::link(new moodle_url('/'), "<h1 class='headermain'><img src='".$PAGE->theme->settings->logo."' alt='logo' width='320px' height='100px'/></h1>");
-									} else { ?>
-									    <div id="heading"><?php echo $PAGE->heading ?>
-										<p class="tagline"><?php echo $tagline ?></p></div>
-									    
-									<?php
-									} ?>
-							    	</div>
-                        		    <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
-		                        </div>
-    		                </div>
-        		        </div>
-
-    	    	        <?php if ($hassidepost) { ?>
-        	    	    <div id="region-post" class="block-region">
-        	    	    	<div id="region-post-wrap-1">
-        	    	    		<div id="region-post-wrap-2">
-		            	    	    <div class="region-content">
-        		        	    	    <?php echo $OUTPUT->blocks_for_region('side-post') ?>
-	            		        	</div>
-		                		</div>
-		                	</div>
-		                </div>
-    		            <?php
-} ?>
-
-            		</div>
-        	    </div>
-	        </div>
-    	</div>
-
-<!-- END OF CONTENT -->
-
-	</div> <!-- END #wrapper -->
-
-		</div>
-	</div>
-
-<!-- START OF FOOTER -->
-    <div id="page-footer" class="wrapper clearfix">
-    <?php echo $footnote ?>
-        <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
-        <?php
-            echo $OUTPUT->login_info();
-            echo $OUTPUT->home_link();
-            echo $OUTPUT->standard_footer_html();
-        ?>
-    </div>
-
-<!-- END OF FOOTER -->
-
-
-</div> <!-- END #page -->
-
-<?php echo $OUTPUT->standard_end_of_body_html() ?>
-</body>
+    <head>
+        <title><?php echo $PAGE->title ?></title>
+        <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme') ?>" />
+        <meta name="description" content="<?php p(strip_tags(format_text($SITE->summary, FORMAT_HTML))) ?>" />
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <?php echo $OUTPUT->standard_head_html() ?>
+    </head>
+    <body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses . ' ' . join(' ', $bodyclasses)) ?>">
+        <?php echo $OUTPUT->standard_top_of_body_html() ?>
+        <div id="page">
+            <div id="page-wrap1">
+                <div id="page-wrap2">
+                    <div id="wrapper" class="clearfix">
+                        <!-- START OF HEADER -->
+                        <div id="page-header">
+                            <div id="page-header-wrapper" class="wrapper clearfix">
+                                <div id="headermenus" class="clearfix">
+                                    <div class="headermenu clearfix">
+                                        <?php
+                                        echo $OUTPUT->lang_menu();
+                                        echo $OUTPUT->login_info();
+                                        echo $PAGE->headingmenu;
+                                        ?>
+                                    </div>
+                                    <?php if ($hascustommenu) { ?>
+                                        <div id="custommenu"><?php echo $custommenu; ?></div>
+                                    <?php } else {
+                                        ?>
+                                        <div id="custommenu" style="line-height:1em;">&nbsp;</div> <!-- temporary until I find a better fix -->
+                                    <?php }
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END OF HEADER -->
+                        <!-- START OF CONTENT -->
+                        <div id="page-content-wrapper" class="wrapper clearfix">
+                            <div id="page-content">
+                                <div id="region-main-box">
+                                    <div id="region-post-box">
+                                        <div id="region-main-wrap">
+                                            <div id="region-main">
+                                                <div class="region-content">
+                                                    <div id="region-header">									
+                                                        <?php
+                                                        if ($haslogo) {
+                                                            echo html_writer::link(new moodle_url('/'), "<h1 class='headermain'><img src='" . $PAGE->theme->settings->logo . "' alt='logo' width='320px' height='100px'/></h1>");
+                                                        } else {
+                                                            ?>
+                                                            <div id="heading"><?php echo $PAGE->heading ?>
+                                                                <p class="tagline"><?php echo $tagline ?></p></div>
+                                                        <?php }
+                                                        ?>
+                                                    </div>
+                                                    <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php if ($hassidepost) { ?>
+                                            <div id="region-post" class="block-region">
+                                                <div id="region-post-wrap-1">
+                                                    <div id="region-post-wrap-2">
+                                                        <div class="region-content">
+                                                            <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        <?php }
+                                        ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END OF CONTENT -->
+                    </div> <!-- END #wrapper -->
+                </div>
+            </div>
+            <!-- START OF FOOTER -->
+            <div id="page-footer" class="wrapper clearfix">
+                <?php echo $footnote ?>
+                <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
+                <?php
+                echo $OUTPUT->login_info();
+                echo $OUTPUT->home_link();
+                echo $OUTPUT->standard_footer_html();
+                ?>
+            </div>
+            <!-- END OF FOOTER -->
+        </div> <!-- END #page -->
+        <?php echo $OUTPUT->standard_end_of_body_html() ?>
+    </body>
 </html>
